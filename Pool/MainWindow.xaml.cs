2455314e7285625af47ebb4f7303e5a629b4b43e
Xtoto1911 <super.mr.kirill@mail.ru>
2023-11-13 22:54:00 +0300

    Забил на бэквокер, делаем через Taskи

--- Pool/MainWindow.xaml.cs
+++ Pool/MainWindow.xaml.cs
@@ -15,6 +15,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Pool
 {
@@ -24,102 +25,116 @@ namespace Pool
     /// 
 
 
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
-        private BackgroundWorker worker = new();
-        private Dictionary<string, Thread> dictTh;
-        private object lockObject = new object();
-        private double maxPoolLevel;
-        private double currPoolLevel;
-        private double jetValue;
+        private byte ups = 10;
+        private int speed = 1;
+        private int waterLvl = 0;
+
+        private bool isWorking = false;
+        private bool isDangerZone = false;
+
+        private List<Pump> listPums = new() 
+        {
+            new(0,0, true),
+            new(1,20),
+            new(2,20),
+            new(3,20),
+            new(4,20),
+            new(5,20),
+        };
+
+        private List<Task> listTasks = new();
+
+        private readonly object lockObj = new object();
+
+        public int WaterLvl
+        {
+            get => waterLvl;
+            set
+            {
+                if (value != waterLvl)
+                {
+                    waterLvl = value;
+                    OnPropertyChanged(nameof(WaterLvl));
+                }
+            }
+        }
+
+
         public MainWindow()
         {
+            DataContext = this;
             InitializeComponent();
-            InitWorker();
-            InitSettings();
-            InitThreads();
-        }
-
-        private void InitWorker()
-        {
-            worker.WorkerReportsProgress = true;
-            worker.WorkerSupportsCancellation = true;
-            worker.DoWork += Worker_DoWork;
-            worker.ProgressChanged += Worker_ProgressChanged;
-            worker.RunWorkerCompleted += Worker_RunWorkerCompleted;
-            worker.RunWorkerAsync();
-        }
-
-        private void InitSettings()
-        {
-            maxPoolLevel = bar.Maximum;
-            jetValue = jet.Width;
-            currPoolLevel = 0;
-        }
-
-        private void InitThreads()
-        {
-            for (int i = 0; i < 5; i++)
-            {
-                dictTh[i.ToString()] = new Thread(DecreaseWorkerThread);
-                dictTh[i.ToString()].Start();
-            }
-        }
-
-        private void Worker_DoWork(object sender, DoWorkEventArgs e)
-        {
-            for (; currPoolLevel <= maxPoolLevel; currPoolLevel += jetValue)
-            {
-                // Задержка для эмуляции длительной работы
-                Thread.Sleep(60);
-
-                // Отправка прогресса в основной поток
-                worker.ReportProgress((int)currPoolLevel);
-            }
-        }
-
-        private void Worker_ProgressChanged(object sender, ProgressChangedEventArgs e)
-        {
-            // Обновление прогресс-бара в основном потоке
-            bar.Value = e.ProgressPercentage;
-        }
-
-        private void Worker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
-        {
-            worker.RunWorkerAsync();
         }
 
 
-        private void DecreaseWorkerThread()
-        {
-            while (true)
-            {
-                Thread.Sleep(200); // Эмуляция работы
-
-                lock (this)
-                {
-                    if (currPoolLevel < maxPoolLevel * 3/4)
-                    {
-                        currPoolLevel -= 2;
-                        if (currPoolLevel < 0)
-                        {
-                            currPoolLevel = 0;
-                        }
-                    }
-                }
-
-                Dispatcher.Invoke(() => bar.Value = currPoolLevel); // Обновление UI в основном потоке
-            }
-        }
-
-        private void sl_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
-        {
-            jetValue =  jet.Width = (double)e.NewValue;
-        }
 
         private void pump1_Click(object sender, RoutedEventArgs e)
         {
+            int currID = int.Parse(((Button)sender).Content.ToString());
+            listPums[currID].IsOn = !listPums[currID].IsOn;
+        }
 
+        private void StartStop_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void StopProgram()
+        {
+            isWorking = false;
+            isDangerZone = false;
+            foreach (var task in listTasks)
+            {
+                task.Wait();
+                task.Dispose();
+            }
+            listTasks.Clear();
+        }
+
+        private void StartProgram()
+        {
+            isWorking = true;   
+            foreach (var pump in listPums)
+            {
+                listTasks.Add(Task.Run(() =>
+                {
+                    while (isWorking)
+                    {
+                        lock (lockObj)
+                        {
+                            if (pump.IsTap)
+                                UpWater(pump);
+                            else
+                                DownWater(pump);
+                        }
+                    }
+                    Thread.Sleep(1000 / (ups * speed));
+                }));
+            }
+        }
+
+        private void UpWater(Pump tap)
+        {
+            if(WaterLvl < bar.Maximum)
+                WaterLvl = tap.Pumping(waterLvl * ups) / ups;
+            if (waterLvl >= bar.Maximum * 3 / 4)
+                isDangerZone = true;
+        }
+
+        private void DownWater(Pump pump)
+        {
+            if (isDangerZone)
+                WaterLvl = pump.Pumping(waterLvl * ups) / ups;
+            if (waterLvl <= bar.Maximum * 1 / 4)
+                isDangerZone = false;
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
---
+++ Pool/Pump.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pool
+{
+    public class Pump
+    {
+        private int ID {  get; set; }
+        private int forse;
+        private int Force
+        {
+            get => forse;
+            set
+            {
+                if ((IsTap && value < 0) || (!IsTap && value > 0))
+                {
+                    forse = -value;
+                    return;
+                }
+                forse = value;
+                
+            }
+        }
+        private bool isOn = true;
+        private bool isTap;
+        public bool IsOn
+        {
+            get => isOn;
+            set => isOn = value;
+        }
+
+        public bool IsTap
+        {
+            get => isTap;
+            set => isTap = value;
+        }
+        public Pump(int id, int force, bool isTap = false)
+        {
+            IsTap = isTap;
+            ID = id;
+            Force = force;
+        }
+
+        public int Pumping(int waterLvL)
+        {
+            return IsOn ? waterLvL + Force : waterLvL;
+        }
+    }
+}
